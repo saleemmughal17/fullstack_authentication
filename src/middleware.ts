e9d74f4d03131ac338479928e3945d4c5c5f0433
saleemmughal17 <saleem177mughal@gmail.com>
2025-02-26 18:39:35 +0500

    Updated dashboard and added pending applications feature

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,27 @@
 import { NextResponse, NextRequest } from "next/server";
 import { cookies } from "next/headers";
+import { getIronSession } from "iron-session";
+import { sessionOptions} from "../lib/session";
 
 
 export async function middleware(req: NextRequest) {
+
+  let res = NextResponse.next()
   const cookieStore = await cookies();
+
+  const session = await getIronSession(req, res, sessionOptions);
+
   const token = cookieStore.get("auth_session");
 
-
+  
+  console.log(session.user)
 
   if (!token) {
     return NextResponse.redirect(new URL("/login", req.url));
+  }
+  const { pathname } = req.nextUrl;
+   if (pathname.startsWith('/admin') && session.user.role !== 'Admin') {
+    return NextResponse.redirect(new URL('/DashboardUser', req.url));
   }
 
 
@@ -22,5 +34,5 @@ export const config = {
   matcher: [
     "/DashboardUser",
      "/admin",
-     "/jobs","/addJob","/applications","/applicationlist","/api/applications","/api/getapplication","/api/jobs","/api/update2"], 
+     "/jobs/:path*","/addJob","/applications","/applicationlist","/api/applications","/api/getapplication","/api/jobs","/api/update2"], 
 };
